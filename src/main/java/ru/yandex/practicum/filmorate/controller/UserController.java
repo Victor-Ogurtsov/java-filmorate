52fb39a854697771ef8cfa2382278a19da2f6777
Victor <vicyar89@yandex.ru>
2024-04-30 12:47:51 +0300

    10-sprint filmorate checkstyle adjustments WhitespaceAround and LeftCurly

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -26,7 +26,9 @@ public class UserController extends BaseController<User> {
     @PostMapping
     public User addUser(@Valid @RequestBody User user) {
         checkUser(user);
-        if (user.getName() == null) {setUserName(user);}
+        if (user.getName() == null) {
+            setUserName(user);
+        }
         user.setId(getNextId(users));
         log.info("Добавлен пользователь с id={}", user.getId());
         users.put(user.getId(), user);
@@ -41,7 +43,9 @@ public class UserController extends BaseController<User> {
         } else if (!users.containsKey(user.getId())) {
             throw new ValidationException("user c id=" + user.getId() + " не найден");
         }
-        if (user.getName() == null) {setUserName(user);}
+        if (user.getName() == null) {
+            setUserName(user);
+        }
         log.info("Изменен пользователь с id={}", user.getId());
         users.put(user.getId(), user);
         return users.get(user.getId());
